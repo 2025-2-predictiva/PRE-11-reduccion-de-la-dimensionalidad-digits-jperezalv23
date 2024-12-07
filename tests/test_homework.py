de60f0ce95fa128fe46fef64212026fc9bf7ca7c
Juan David Velásquez-Henao <jdvelasq@unal.edu.co>
2024-12-07 08:09:57 -0500

    Update test_homework.py

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -5,6 +5,6 @@ import os
 
 def test_01():
     """Check if the files are present"""
-    assert os.path.exists("digits_pca.png")
-    assert os.path.exists("digits_tsne.png")
-    assert os.path.exists("digits_umap.png")
+    assert os.path.exists("homework/digits_pca.png")
+    assert os.path.exists("homework/digits_tsne.png")
+    assert os.path.exists("homework/digits_umap.png")
